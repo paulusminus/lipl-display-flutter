37c48978f52f238fa584d93917efdccdd9aa50ba
paulusminus <info@paulmin.nl>
2024-03-02 10:15:41 +0100

    Feature: start lipl-gatt-bluer-cli

--- lib/main.dart
+++ lib/main.dart
@@ -2,42 +2,43 @@ import 'dart:io';
 import 'dart:convert';
 import 'dart:math';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lipl_display/lipl_display.dart';
-// import 'ffi.dart';
+import 'package:logging/logging.dart';
 
-// const String defaultPart = "Even geduld a.u.b.";
-// const double defaultFontsize = 30.0;
-// const double defaultFontsizeIncrement = 3.0;
-// const bool defaultDarkmode = false;
+void main() async {
+  Logger.root.onRecord.listen((record) {
+    if (kDebugMode) {
+      print('${record.level.name}: ${record.time}: ${record.message}');
+    }
+  });
+  final log = Logger('lipl_display');
 
-void main() {
-  // final messages = peripheral.gattListen(
-  //   part: defaultPart,
-  //   status: '',
-  //   dark: defaultDarkmode,
-  //   fontSize: defaultFontsize,
-  //   fontSizeIncrement: defaultFontsizeIncrement,
-  // );
-
-  final f = File('/home/paul/Code/rust/lipl-display/lipl-out').openRead();
-  final messages = f
+  final gatt = await Process.start("lipl-gatt-bluer-cli", []);
+  final messages = gatt.stdout
       .transform(const Utf8Decoder())
       .transform(const LineSplitter())
       .map(jsonDecode)
       .map((json) => LiplDisplay.fromJson(json))
       .asBroadcastStream();
 
-  messages.listen(print);
+  messages.listen(log.info,
+      onDone: () => {
+            Future.delayed(const Duration(seconds: 1))
+                .then((value) => SystemNavigator.pop())
+          });
 
   runApp(
-    DisplayApp(messages: messages),
+    DisplayApp(messages: messages, log: log),
   );
 }
 
 class DisplayApp extends StatelessWidget {
-  const DisplayApp({super.key, required this.messages});
+  const DisplayApp({super.key, required this.messages, required this.log});
   final Stream<LiplDisplay> messages;
+  final Logger log;
 
   @override
   Widget build(BuildContext context) {
