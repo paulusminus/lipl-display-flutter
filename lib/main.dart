fa37e910333b3368bc0214e381e205934673efe9
paulusminus <info@paulmin.nl>
2024-03-05 23:58:36 +0100

    Changes: Set defaults on child process

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'dart:math';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:lipl_display/lipl_display.dart';
 import 'package:logging/logging.dart';
+
+const int defaultFontsize = 30;
+const bool defaultDark = true;
 
 void main() async {
   Logger.root.onRecord.listen((record) {
@@ -16,23 +18,91 @@ void main() async {
   });
   final log = Logger('lipl_display');
 
-  final gatt = await Process.start("lipl-gatt-bluer-cli", []);
+  final gatt = await Process.start(
+      "lipl-gatt-bluer-cli", ["-d", "$defaultDark", "-f", "$defaultFontsize"]);
   final messages = gatt.stdout
       .transform(const Utf8Decoder())
       .transform(const LineSplitter())
       .map(jsonDecode)
-      .map((json) => LiplDisplay.fromJson(json))
-      .asBroadcastStream();
-
-  messages.listen(log.info,
-      onDone: () => {
-            Future.delayed(const Duration(seconds: 1))
-                .then((value) => SystemNavigator.pop())
-          });
+      .map((json) => LiplDisplay.fromJson(json));
 
   runApp(
     DisplayApp(messages: messages, log: log),
   );
+}
+
+class ShowError extends StatelessWidget {
+  const ShowError({super.key, required this.error});
+  final Object? error;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Error: ${error ?? "unknown"}')),
+    );
+  }
+}
+
+class ShowMessage extends StatelessWidget {
+  const ShowMessage({super.key, required this.message});
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('No more data'),
+      ),
+    );
+  }
+}
+
+class ShowData extends StatelessWidget {
+  const ShowData({super.key, required this.liplDisplay});
+  final LiplDisplay liplDisplay;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final padding = min(size.width, size.height) * 0.05;
+
+    return Scaffold(
+      backgroundColor: liplDisplay.dark ? Colors.black : Colors.white,
+      body: Padding(
+        padding: EdgeInsets.all(padding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Text(
+                liplDisplay.text,
+                style: TextStyle(
+                  height: 1.2,
+                  fontSize: liplDisplay.fontSize,
+                  color: liplDisplay.dark ? Colors.white : Colors.black,
+                ),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: liplDisplay.fontSize),
+                  child: Text(
+                    liplDisplay.status,
+                    style: TextStyle(
+                      fontSize: liplDisplay.fontSize * 0.7,
+                      color: liplDisplay.dark ? Colors.white : Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class DisplayApp extends StatelessWidget {
@@ -48,59 +118,24 @@ class DisplayApp extends StatelessWidget {
       home: StreamBuilder<LiplDisplay>(
         stream: messages,
         builder: (context, snapshot) {
-          final size = MediaQuery.of(context).size;
-          final padding = min(size.width, size.height) * 0.05;
-
-          if (snapshot.connectionState == ConnectionState.done) {
-            return const Scaffold(
-              body: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
+          if (snapshot.hasError) {
+            return ShowError(error: snapshot.error);
+          } else {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return const ShowMessage(message: 'No More Data');
+              case ConnectionState.active:
+                return ShowData(liplDisplay: snapshot.data!);
+              case ConnectionState.none:
+                return const ShowMessage(
+                  message: 'None',
+                );
+              case ConnectionState.waiting:
+                return const ShowMessage(
+                  message: 'Waiting',
+                );
+            }
           }
-          return Scaffold(
-            backgroundColor:
-                snapshot.data?.dark ?? false ? Colors.black : Colors.white,
-            body: Padding(
-              padding: EdgeInsets.all(padding),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Text(
-                      snapshot.data?.text ?? '',
-                      style: TextStyle(
-                        height: 1.2,
-                        fontSize: snapshot.data?.fontSize,
-                        color: snapshot.data?.dark ?? false
-                            ? Colors.white
-                            : Colors.black,
-                      ),
-                    ),
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: snapshot.data?.fontSize ?? 8.0),
-                        child: Text(
-                          snapshot.data?.status ?? '',
-                          style: TextStyle(
-                            fontSize: snapshot.data == null
-                                ? null
-                                : snapshot.data!.fontSize * 0.7,
-                            color: snapshot.data?.dark ?? false
-                                ? Colors.white
-                                : Colors.black,
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          );
         },
       ),
     );
